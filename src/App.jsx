27753da9a901bee1f26066584435c95298cef3f0
Oğuzhan Yıldırır <84236077+oyldrr@users.noreply.github.com>
2025-01-07 19:07:52 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PageContainer from './container/PageContainer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <PageContainer>
+      <Header />
+    </PageContainer>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+// Custom CSS
+import '../css/Header.css'
+
+// React Icons
+import { CiShoppingBasket } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+import { IoMoonOutline } from "react-icons/io5";
+
+
+function Header() {
+
+    const [theme, setTheme] = useState("false");
+
+    const changeTheme = () => {
+        const root = document.getElementById("root");
+        setTheme(!theme);
+        if (theme) {
+            root.style.backgroundColor = "#000";
+            root.style.color = "#fff";
+        }
+        else {
+            root.style.backgroundColor = "#fff";
+            root.style.color = "#000"
+        }
+    }
+
+    return (
+        <div className='flex-space-between'>
+            <div className='flex-row'>
+                <img className='logo' src="./src/images/logo.png" alt="logo" />
+                <p className='logo-text'>ouz co.</p>
+            </div>
+            <div className="flex-row">
+                <div>
+                    <input className='search-input' type="text" placeholder='Search...' />
+                </div>
+                <div>
+                    {
+                        theme ? <IoMoonOutline onClick={changeTheme} className='icon' /> : <CiLight className='icon' onClick={changeTheme} />
+                    }
+                    <CiShoppingBasket className='icon' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
